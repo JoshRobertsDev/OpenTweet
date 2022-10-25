454aa56b71b3a5af7ca92ba6b03e7992e14294ef
Josh Roberts <josh.roberts@yell.com>
2022-10-25 23:03:22 +0100

    Add contentRunAttributes to extract mentions and links

--- OpenTweet/ViewModels/TweetViewModel.swift
+++ OpenTweet/ViewModels/TweetViewModel.swift
@@ -15,6 +15,7 @@ class TweetViewModel: ObservableObject, Identifiable {
     let id: String
     let author: String
     let content: String
+    let contentRunAttributes: [ContentRunAttributes]
     let avatar: URL?
     let datePosted: String
     let replyingToTweet: String?
@@ -32,9 +33,39 @@ class TweetViewModel: ObservableObject, Identifiable {
     init(tweet: TweetDataModel, allTweets: [TweetDataModel], tweetService: TweetService) {
         self.id = tweet.id
         self.author = tweet.author
-        self.content = tweet.content
         self.avatar = tweet.avatar
-        self.replyingTo = allTweets.first { $0.id == tweet.inReplyTo }?.author
+        
+        let replyingTo = allTweets.first { $0.id == tweet.inReplyTo }?.author
+        
+        var content = tweet.content
+        
+        if let replyingTo = replyingTo, content.hasPrefix(replyingTo + " ") {
+            content = String(content.dropFirst(replyingTo.count + 1))
+        }
+        
+        self.content = content
+        
+        var contentRunAttributes = [ContentRunAttributes]()
+        
+        if let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue) {
+            let range = NSRange(location: 0, length: content.count)
+            
+            detector
+                .matches(in: content, options: [], range: range)
+                .forEach { contentRunAttributes.append(ContentRunAttributes.link(range: $0.range)) }
+        }
+        
+        if let replyingTo = replyingTo, let regex = try? NSRegularExpression(pattern: replyingTo) {
+            let range = NSRange(content.startIndex..<content.endIndex, in: content)
+            
+            regex
+                .matches(in: content, range: range)
+                .forEach { contentRunAttributes.append(ContentRunAttributes.mention(range: $0.range)) }
+        }
+        
+        self.contentRunAttributes = contentRunAttributes
+        
+        self.replyingTo = replyingTo
         self.replyingToTweet = allTweets.first { $0.id == tweet.inReplyTo }?.id
         self.repliesCount = allTweets.filter { $0.inReplyTo == tweet.id }.count
         
@@ -74,6 +105,14 @@ class TweetViewModel: ObservableObject, Identifiable {
                 self.replies = .fetched(viewModels)
             })
             .store(in: &tokens)
+    }
+}
+
+extension TweetViewModel {
+    
+    enum ContentRunAttributes {
+        case link(range: NSRange)
+        case mention(range: NSRange)
     }
 }
 
