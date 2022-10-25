6d6b341605abc38407bf24a4b1d5ed4cad9a0ee3
Josh Roberts <josh.roberts@yell.com>
2022-10-25 22:23:00 +0100

    Add initial tweet detail implementation

---
+++ OpenTweet/Views/TweetDetailView.swift
@@ -0,0 +1,63 @@
+//
+//  TweetDetailView.swift
+//  OpenTweet
+//
+//  Created by Josh Roberts on 25/10/2022.
+//
+
+import SwiftUI
+
+struct TweetDetailView: View {
+    
+    @ObservedObject var tweet: TweetViewModel
+    
+    init(tweet: TweetViewModel) {
+        self.tweet = tweet
+        tweet.fetchTweetReplies()
+    }
+    
+    var body: some View {
+        List {
+            Section {
+                VStack(alignment: .leading, spacing: 8) {
+                    HStack(spacing: 16) {
+                        AvatarView(url: tweet.avatar)
+                        TweetAuthorView(author: tweet.author, datePosted: tweet.datePosted)
+                    }
+                    
+                    TweetContentView(content: tweet.content)
+                        .font(.title)
+                    
+                    TweetInfoView(repliesCount: tweet.repliesCount)
+                        .padding(.top, 8)
+                }
+            }
+            
+            Section {
+                ForEach(tweet.replies) { tweetReply in
+                    InvisibleNavigationLink(destination: TweetDetailView(tweet: tweetReply)) {
+                        TweetView(tweet: tweetReply)
+                    }
+                }
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct TweetDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetDetailView(tweet: TweetViewModel(
+            tweet: MockData.timeline[0],
+            allTweets: MockData.timeline,
+            tweetService: TweetService()
+        ))
+        
+        TweetDetailView(tweet: TweetViewModel(
+            tweet: MockData.timeline[1],
+            allTweets: MockData.timeline,
+            tweetService: TweetService()
+        ))
+    }
+}
